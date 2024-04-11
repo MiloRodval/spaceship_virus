740be81936006cae0a1e60c75351e92a357eae08
Milo Rodval <camilocrvrv98@gmail.com>
2024-04-10 22:58:29 -0300

    added a function called from_word_to_list and implemented it

---
+++ functions.py
@@ -0,0 +1,5 @@
+def from_word_to_list(word: int)-> list:
+    list = []
+    for letter in word:
+        list.append(letter)
+    return list
--- game.py
+++ game.py
@@ -1,21 +1,15 @@
 from random import choices
+from functions import from_word_to_list
 
 alphabet: str = 'qwertyuiopasdfghjklzxcvbnm'
-list_alphabet: list = []
-user_input_list: list = []
-
-for l in alphabet:
-    list_alphabet.append(l)
-
+list_alphabet: list = from_word_to_list(alphabet)
 random_letters: list = choices(list_alphabet, k=5)
 
 for l in random_letters:
     print(l, end='')
+print()
 
-user_input = input('Your turn: ')
-
-for x in user_input:
-    user_input_list.append(x)
+user_input_list: list = from_word_to_list(input('Your turn: '))
 
 if user_input_list == random_letters:
     print('You win!')
