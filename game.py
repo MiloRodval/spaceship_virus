ce8fa63e2c5302cd7c656ae15b86713da4fcb7eb
Milo Rodval <camilocrvrv98@gmail.com>
2024-04-10 22:47:47 -0300

    first functionality prototype

--- game.py
+++ game.py
@@ -1,9 +1,23 @@
-from random import randint, shuffle
+from random import choices
 
 alphabet: str = 'qwertyuiopasdfghjklzxcvbnm'
-list_alphabet = []
+list_alphabet: list = []
+user_input_list: list = []
 
 for l in alphabet:
     list_alphabet.append(l)
 
-print(shuffle(list_alphabet))
+random_letters: list = choices(list_alphabet, k=5)
+
+for l in random_letters:
+    print(l, end='')
+
+user_input = input('Your turn: ')
+
+for x in user_input:
+    user_input_list.append(x)
+
+if user_input_list == random_letters:
+    print('You win!')
+else:
+    print('You are a looser')
