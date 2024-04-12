03b475f9f87b91744cbdba7bfeaf48815f922bd9
Milo Rodval <camilocrvrv98@gmail.com>
2024-04-11 21:13:01 -0300

    added/defined main in game.py

--- game.py
+++ game.py
@@ -1,17 +1,21 @@
 from random import choices
 from functions import from_word_to_list
 
-alphabet: str = 'qwertyuiopasdfghjklzxcvbnm'
-list_alphabet: list = from_word_to_list(alphabet)
-random_letters: list = choices(list_alphabet, k=5)
+def main():
+    alphabet: str = 'qwertyuiopasdfghjklzxcvbnm'
+    list_alphabet: list = from_word_to_list(alphabet)
+    random_letters: list = choices(list_alphabet, k=5)
 
-for l in random_letters:
-    print(l, end='')
-print()
+    for l in random_letters:
+        print(l, end='')
+    print()
 
-user_input_list: list = from_word_to_list(input('Your turn: '))
+    user_input_list: list = from_word_to_list(input('Your turn: '))
 
-if user_input_list == random_letters:
-    print('You win!')
-else:
-    print('You are a looser')
+    if user_input_list == random_letters:
+        print('You win!')
+    else:
+        print('You are a looser')
+
+if __name__ == "__main__":
+    main()
