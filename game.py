bb2a5412ee8f3ae4513e7ef001c33fbe750bd950
MiloRodval <camilocrvrv98@gmail.com>
2024-05-01 22:47:39 -0300

    added world class and made code more readible

--- game.py
+++ game.py
@@ -1,59 +1,86 @@
 from objects.spaceship import Spaceship
 from objects.spaceship import Enemy
 from objects.jank import Jank
+from objects.world import World
+from objects.world import Screen
 import pygame
+import copy
 import random
 
 # BUGS
 # En la segunda o tercera BOX el enemy no muere
 # A veces aparece el enemigo en la coordenada 0, 0
+# Estoy mostrando las balas SOLO SI el enemigo esta vivo; cuando muere, no aparecen
 
+pygame.font.init()
 pygame.display.set_caption('Spaceship Virus')
-WIDTH = 1300
-HEIGHT = 700
-BOX = round(HEIGHT / 13)
-# Remember to divide Y position and subtract half of the spaceship height
-player = Spaceship(speed=BOX, height=BOX, position={'x_position': 0, 'y_position': BOX*5})
-enemy = [Enemy(speed=BOX, height=BOX, position={'x_position': 1200, 'y_position': random.randint(2, 11) * BOX})]
-# This flag takes borders out
-flags = pygame.NOFRAME
-screen = pygame.display.set_mode((WIDTH, HEIGHT), flags)
-background = pygame.transform.scale(pygame.image.load('interface/images/space_background.png'), (WIDTH, BOX*10)).convert()
+
+world = World(
+    width = 1300,
+    height = 400,
+    background_image = 'interface/images/space_background.png'
+)
+
+screen = Screen(
+
+)
+
+player = Spaceship(
+    speed = world.box,
+    height = world.box,
+    position = {
+        'x_position': 0,
+        'y_position': world.box*5
+        }
+)
+
+enemy = [Enemy(
+    speed = world.box,
+    height = world.box,
+    position = {
+        'x_position': 1200,
+        'y_position': random.randint(2, 11) * world.box
+    }
+    )]
+
+game_screen = world.get_game_screen()
 clock = pygame.time.Clock()
 previous_bullet_position = {}
 
-
 def draw(spaceship_bullets):
 
-    # Draw background, player and enemy
-    screen.blit(background, (0, BOX*2))
-    player.blit(screen)
-    # Solo lo estoy dejando de mostrar
+    dialogue_screen = pygame.draw.rect(game_screen, (255, 255, 255), pygame.Rect(0, 0, world.width, world.box))
+    game_screen.blit(world.get_background(), (0, world.box*2))
+    player.blit(game_screen)
+
     if not enemy == []:
-        enemy[0].blit(screen)
+        enemy[0].blit(game_screen)
 
     # Draw spaceship bullets
-    if not spaceship_bullets == []:
-        for bullet in spaceship_bullets:
-            if not enemy == []:
-                if bullet.position == enemy[0].position:
-                    del bullet
-                    del enemy[0]
-                else:
-                    bullet.blit(screen)
-                    previous_bullet_position[bullet] = bullet.position
+    for bullet in spaceship_bullets:
+        if not enemy == []:
+            if bullet.position == enemy[0].position:
+                spaceship_bullets.remove(bullet)
+                del enemy[0]
+            else:
+                bullet.blit(game_screen)
+                previous_bullet_position[bullet] = bullet.position
+
+    game_screen.blit(screen.get_screen_text(), dialogue_screen)
             
     pygame.display.update()
 
 
 def main():
+
     running: bool = True
-    spaceship_bullets: list = []
     enemy_moved: bool = False
+    space_pressed: bool = False
+    spaceship_bullets: list = []
 
     while running:
 
-        clock.tick(60)        
+        clock.tick(30)        
 
         # If player does something...
         for event in pygame.event.get():
@@ -70,13 +97,11 @@ def main():
             if keys[pygame.K_DOWN]:
                 player.move_down()
             if keys[pygame.K_SPACE]:
-                beam = (
-                    Jank(height=BOX, speed=40, position={
-                        'x_position': player.position['x_position'] + player.position['x_position'] / 2,
-                        'y_position': player.position['y_position']
-                    })
-                )
-                spaceship_bullets.append(beam)
+                spaceship_bullets.append(
+                    Jank(height=world.box, speed=40, position={
+                    'x_position': player.position['x_position'] + player.position['x_position'] / 2,
+                    'y_position': player.position['y_position']
+                }))
 
             if keys[pygame.K_w] and not enemy_moved:
                 enemy[0].move_up()
@@ -96,7 +121,7 @@ def main():
 
         if not spaceship_bullets == []:
             for bullet in spaceship_bullets:
-                bullet.move_to_right(WIDTH, spaceship_bullets)
+                bullet.move_to_right(world.width, spaceship_bullets)
                 if not enemy == []:
                     if enemy[0].position['x_position'] == round(bullet.position['x_position']) and enemy[0].position['y_position'] == round(bullet.position['y_position']):
                         enemy[0].life = 0
---
+++ objects/world.py
@@ -0,0 +1,28 @@
+import pygame
+
+class World:
+    def __init__(self, width, height, background_image):
+        self.width = width
+        self.height = height
+        self.box = round(self.height / 13)
+        self.background_image = background_image
+
+    def get_background(self):
+        return pygame.transform.scale(pygame.image.load(self.background_image), (self.width, self.box*10)).convert()
+    
+    def take_borders_out(self):
+        return pygame.NOFRAME
+
+    def get_game_screen(self):
+        return pygame.display.set_mode((self.width, self.height), self.take_borders_out())
+    
+class Screen:
+
+    def get_user_input(self):
+        return input()
+    
+    def get_screen_font(self):
+        return pygame.font.Font(None, 32)
+    
+    def get_screen_text(self):
+        return self.get_screen_font().render('Hello', True, (0, 255, 255))
